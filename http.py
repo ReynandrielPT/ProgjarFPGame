c0f68526b0a524c44e018e14e2c96e1cfc5b919f
Reynandriel <162027180+ReynandrielPT@users.noreply.github.com>
2025-06-19 22:39:48 +0700

    Update http.py

--- http.py
+++ http.py
@@ -1,122 +1,61 @@
-import sys
-import os.path
-import uuid
-from glob import glob
-from datetime import datetime
+# http.py
+
+import socket
+import threading
 
 class HttpServer:
-	def __init__(self):
-		self.sessions={}
-		self.types={}
-		self.types['.pdf']='application/pdf'
-		self.types['.jpg']='image/jpeg'
-		self.types['.txt']='text/plain'
-		self.types['.html']='text/html'
-	def response(self,kode=404,message='Not Found',messagebody=bytes(),headers={}):
-		tanggal = datetime.now().strftime('%c')
-		resp=[]
-		resp.append("HTTP/1.0 {} {}\r\n" . format(kode,message))
-		resp.append("Date: {}\r\n" . format(tanggal))
-		resp.append("Connection: close\r\n")
-		resp.append("Server: myserver/1.0\r\n")
-		resp.append("Content-Length: {}\r\n" . format(len(messagebody)))
-		for kk in headers:
-			resp.append("{}:{}\r\n" . format(kk,headers[kk]))
-		resp.append("\r\n")
+    def __init__(self, host='127.0.0.1', port=8000):
+        self.host = host
+        self.port = port
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print(f"HTTP Server listening on {self.host}:{self.port}")
 
-		response_headers=''
-		for i in resp:
-			response_headers="{}{}" . format(response_headers,i)
-		#menggabungkan resp menjadi satu string dan menggabungkan dengan messagebody yang berupa bytes
-		#response harus berupa bytes
-		#message body harus diubah dulu menjadi bytes
-		if (type(messagebody) is not bytes):
-			messagebody = messagebody.encode()
+    def start(self):
+        try:
+            while True:
+                client_socket, client_address = self.server_socket.accept()
+                handler_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+                handler_thread.daemon = True
+                handler_thread.start()
+        except KeyboardInterrupt:
+            print("Server shutting down.")
+            self.server_socket.close()
 
-		response = response_headers.encode() + messagebody
-		#response adalah bytes
-		return response
+    def handle_client(self, client_socket):
+        try:
+            request_data = client_socket.recv(4096).decode('utf-8')
+            if not request_data:
+                client_socket.close()
+                return
 
-	def proses(self,data):
-		
-		requests = data.split("\r\n")
-		#print(requests)
+            lines = request_data.split('\r\n')
+            request_line = lines[0]
+            method, path, _ = request_line.split(' ')
 
-		baris = requests[0]
-		#print(baris)
+            body = ""
+            if method == "POST":
+                try:
+                    body_start_index = request_data.find('\r\n\r\n') + 4
+                    body = request_data[body_start_index:]
+                except ValueError:
+                    pass
+            
+            response = self.handle_request(method, path, body)
+            client_socket.sendall(response)
+        except Exception as e:
+            print(f"Error handling client: {e}")
+        finally:
+            client_socket.close()
 
-		all_headers = [n for n in requests[1:] if n!='']
-
-		j = baris.split(" ")
-		try:
-			method=j[0].upper().strip()
-			if (method=='GET'):
-				object_address = j[1].strip()
-				return self.http_get(object_address, all_headers)
-			if (method=='POST'):
-				object_address = j[1].strip()
-				return self.http_post(object_address, all_headers)
-			else:
-				return self.response(400,'Bad Request','',{})
-		except IndexError:
-			return self.response(400,'Bad Request','',{})
-	def http_get(self,object_address,headers):
-		files = glob('./*')
-		#print(files)
-		thedir='./'
-		if (object_address == '/'):
-			return self.response(200,'OK','Ini Adalah web Server percobaan',dict())
-
-		if (object_address == '/video'):
-			return self.response(302,'Found','',dict(location='https://youtu.be/katoxpnTf04'))
-		if (object_address == '/santai'):
-			return self.response(200,'OK','santai saja',dict())
-
-
-		object_address=object_address[1:]
-		if thedir+object_address not in files:
-			return self.response(404,'Not Found','',{})
-		fp = open(thedir+object_address,'rb') #rb => artinya adalah read dalam bentuk binary
-		#harus membaca dalam bentuk byte dan BINARY
-		isi = fp.read()
-		
-		fext = os.path.splitext(thedir+object_address)[1]
-		content_type = self.types[fext]
-		
-		headers={}
-		headers['Content-type']=content_type
-		
-		return self.response(200,'OK',isi,headers)
-	def http_post(self,object_address,headers):
-		headers ={}
-		isi = "kosong"
-		return self.response(200,'OK',isi,headers)
-		
-			 	
-#>>> import os.path
-#>>> ext = os.path.splitext('/ak/52.png')
-
-if __name__=="__main__":
-	httpserver = HttpServer()
-	d = httpserver.proses('GET testing.txt HTTP/1.0')
-	print(d)
-	d = httpserver.proses('GET donalbebek.jpg HTTP/1.0')
-	print(d)
-	#d = httpserver.http_get('testing2.txt',{})
-	#print(d)
-#	d = httpserver.http_get('testing.txt')
-#	print(d)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def handle_request(self, method, path, body):
+        response_body = "<h1>501 Not Implemented</h1>"
+        response_headers = (
+            "HTTP/1.1 501 Not Implemented\r\n"
+            "Content-Type: text/html\r\n"
+            f"Content-Length: {len(response_body)}\r\n"
+            "Connection: close\r\n\r\n"
+        )
+        return (response_headers + response_body).encode('utf-8')
